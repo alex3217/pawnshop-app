deee0d688d4e3951192175509ba7caef10e2134c
Alexander Wash <brandonwash16@icloud.com>
2026-06-02 10:28:25 -0500

    Add PawnShop geo fields

---
+++ apps/api/backend/prisma/migrations/20260602152648_add_pawnshop_geo_fields/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "PawnShop" ADD COLUMN     "city" TEXT,
+ADD COLUMN     "latitude" DOUBLE PRECISION,
+ADD COLUMN     "longitude" DOUBLE PRECISION,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "zip" TEXT;
